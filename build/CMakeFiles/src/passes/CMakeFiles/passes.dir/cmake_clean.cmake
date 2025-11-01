1e02a7e311ff52b2a7d647106febf364cab7ffc8
郑鹏超 <88242806+JHMKA@users.noreply.github.com>
2025-11-01 23:47:22 +0800

    Add files via upload

---
+++ build/CMakeFiles/src/passes/CMakeFiles/passes.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "../../libpasses.a"
+  "../../libpasses.pdb"
+  "CMakeFiles/passes.dir/ConstPropagation.cpp.o"
+  "CMakeFiles/passes.dir/ConstPropagation.cpp.o.d"
+  "CMakeFiles/passes.dir/DeadCode.cpp.o"
+  "CMakeFiles/passes.dir/DeadCode.cpp.o.d"
+  "CMakeFiles/passes.dir/Dominators.cpp.o"
+  "CMakeFiles/passes.dir/Dominators.cpp.o.d"
+  "CMakeFiles/passes.dir/FuncInfo.cpp.o"
+  "CMakeFiles/passes.dir/FuncInfo.cpp.o.d"
+  "CMakeFiles/passes.dir/FunctionInline.cpp.o"
+  "CMakeFiles/passes.dir/FunctionInline.cpp.o.d"
+  "CMakeFiles/passes.dir/Mem2Reg.cpp.o"
+  "CMakeFiles/passes.dir/Mem2Reg.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
